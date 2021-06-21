9b220fbe8383a8dd4f7f7bf24dd060fb760f5a5f
Fisny <bardi.fisniku@gmail.com>
2021-06-21 11:28:13 +0200

    correzione ottavio

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -8,51 +8,66 @@
 // in giallo la riga corrispondente alla squadra
 // con più falli subiti.
 
-const squadreArray = [
-    {
-        nome: 'Sampdoria',
-        punti: 0,
-        falli: 0
-    },
+const teams = [
     {
         nome: 'Inter',
-        punti: 0,
-        falli: 0
+        score: 10,
+        fouls: 1
     },
     {
-        nome: 'Juve',
-        punti: 0,
-        falli: 0
+        nome: 'Juventus',
+        score: 5,
+        fouls: 7
+    },
+    {
+        nome: 'Taranto',
+        score: 7,
+        fouls: 12
     }
-]
+];
 
-const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-let squadraGialla = {
-    nome: "",
-    punti: 0,
-    falli: 0,
-}
+const tableHTML = document.querySelector('.table-container');
 
-for (let x = 0; x < squadreArray.length; x++) {
-    squadreArray[x].punti = randomNumber (0,100);
-    squadreArray[x].falli = randomNumber (0,100);
-    
-    if(squadreArray[x].falli > squadraGialla.falli) {
-        squadraGialla.nome = squadreArray[x].nome;
-        squadraGialla.falli = squadreArray[x].falli;
-        squadraGialla.punti = squadreArray[x].punti;
+
+// di volta in volta inserisco l'ultimo numero più elevato
+// che "ho incontrato" nel ciclo, di falli subiti.
+let lastHigherFouls = 0;
+
+// inserisco l'ultima squadra con più falli subiti che ho
+// incontrato negli step del ciclo
+let lastHigherFoulsTeam;
+
+for (let x = 0; x < teams.length; x++) {
+
+    // il team attuale ha un numero maggiore di falli subiti
+    // rispetto all'ultima osservazione?
+    if (teams[x].fouls > lastHigherFouls) {
+        lastHigherFouls = teams[x].fouls;
+        lastHigherFoulsTeam = teams[x];
     }
+
 }
 
-for (let x = 0; x < squadreArray.length; x++) {
-    const { nome, punti, falli} = squadreArray[x];
-  if(squadraGialla.falli > squadreArray) {
-    const ul = "<ul style=background:yellow></ul>"
-  } else {
-    const ul = `<ul> <li> ${nome} </li> <li> ${punti} </li> <li> ${falli} </li> </ul>`
-    document.getElementById('table-container').innerHTML += ul
-  }
-   
-}
+// ----
 
+for (let x = 0; x < teams.length; x++) {
+
+    const { nome, score, fouls } = teams[x];
+
+
+    let customClass = '';
+    // ma se per caso...
+    if (teams[x] === lastHigherFoulsTeam) {
+        customClass = 'yellow';
+    }
+
+    tableHTML.innerHTML += `
+        <ul class="${customClass}">
+            <li>${nome}</li>
+            <li>${score}</li>
+            <li>${fouls}</li>
+        </ul>
+    `;
+
+}
